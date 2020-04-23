ab1106e3b58f528b861a67d519d0031326d6e05a
YesSkyscrapers <45424671+YesSkyscrapers@users.noreply.github.com>
2020-04-23 10:38:18 +0300

    Update YandexAppmetricaModule.java

--- android/src/main/java/com/codeard/yandexmetrica/YandexAppmetricaModule.java
+++ android/src/main/java/com/codeard/yandexmetrica/YandexAppmetricaModule.java
@@ -226,7 +226,51 @@ public class YandexAppmetricaModule extends ReactContextBaseJavaModule {
          YandexMetrica.reportUserProfile(userProfileBuilder.build());
      }
 
+
+     private JSONObject convertReadableMapToJsonWithMapToJsonParser(final ReadableMap readableMap){
+
+
+         ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+         JSONObject json = new JSONObject();
+
+         try {
+             while (iterator.hasNextKey()) {
+                 String key = iterator.nextKey();
+
+                 switch (readableMap.getType(key)) {
+                     case Null:
+                         json.put(key, null);
+                         break;
+                     case Boolean:
+                         json.put(key, readableMap.getBoolean(key));
+                         break;
+                     case Number:
+                         json.put(key, readableMap.getDouble(key));
+                         break;
+                     case String:
+                         json.put(key, readableMap.getString(key));
+                         break;
+                     case Array:
+                         json.put(key, readableMap.getArray(key));
+                         break;
+                     case Map:
+                         json.put(key, convertReadableMapToJsonWithMapToJsonParser(readableMap.getMap(key)));
+                         break;
+                     default:
+                         break;
+                 }
+             }
+         } catch (Exception ex) {
+             Log.d(TAG, "convertReadableMapToJson fail: " + ex);
+         }
+
+         return json;
+     }
+
     private String convertReadableMapToJson(final ReadableMap readableMap) {
+
+
+
         ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
         JSONObject json = new JSONObject();
 
@@ -251,7 +295,7 @@ public class YandexAppmetricaModule extends ReactContextBaseJavaModule {
                         json.put(key, readableMap.getArray(key));
                         break;
                     case Map:
-                        json.put(key, convertReadableMapToJson(readableMap.getMap(key)));
+                        json.put(key, convertReadableMapToJsonWithMapToJsonParser(readableMap.getMap(key)));
                         break;
                     default:
                         break;
