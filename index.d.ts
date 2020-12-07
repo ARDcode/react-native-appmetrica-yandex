0741154f6bb25b49bbe5e4ba355e68c426c139b2
David <4661784+retyui@users.noreply.github.com>
2020-12-07 23:03:40 +0200

    Update lib definition

--- index.d.ts
+++ index.d.ts
@@ -7,30 +7,31 @@ declare module 'react-native-appmetrica-yandex' {
         firstActivationAsUpdate?: boolean,
     };
 
-    type userProfileConfig = {
+    type UserProfileConfig = {
         name?: string,
         gender?: 'female' | 'male' | string | void,
         age?: number,
         birthDate?: Date | [number] | [number, number] | [number, number, number] | void,
         notificationsEnabled?: boolean,
+    } & {
         [key: string]: string | number | boolean,
     }
 
     export class YandexMetrica extends React.Component {
-        public static activateWithApiKey(apiKey: string);
+        public static activateWithApiKey(apiKey: string): void;
 
-        public static activateWithConfig(params: ActivationConfig);
+        public static activateWithConfig(params: ActivationConfig): void;
 
-        public static reportEvent(message: string);
+        public static reportEvent(message: string): void;
 
-        public static reportEvent(message: string, params: Object);
+        public static reportEvent(message: string, params: object): void;
 
-        public static reportError(name: string, exception?: string | Object);
+        public static reportError(name: string, exception?: string | object): void;
 
-        public static setUserProfileAttributes(params: userProfileConfig);
+        public static setUserProfileAttributes(params: UserProfileConfig): void;
 
-        public static setUserProfileID(userProfileId: string);
+        public static setUserProfileID(userProfileId: string): void;
 
-        public static sendEventsBuffer();
+        public static sendEventsBuffer(): void;
     }
 }
